c661aff1bc2b64b04e7ffd0c9acce08d984095ce
friman15889 <friman.vilcapoma@tecsup.edu.pe>
2019-12-20 13:05:56 -0500

    Tercer Commit

---
+++ app/src/main/java/com/pasantias/redriapp/activities/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.pasantias.redriapp.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pasantias.redriapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        tv=(TextView)findViewById(R.id.tv);
+        iv=(ImageView)findViewById(R.id.iv);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        final Intent i = new Intent(this,RegisterActivity.class);
+        Thread time = new Thread(){
+            public void run (){
+                try{
+                    sleep(4000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+
+                }
+            }
+        };
+        time.start();
+
+    }
+}
